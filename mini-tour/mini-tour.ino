368936e19051787be802c2638eafb7ed33ebb434
B.H <79019374+Baba-29@users.noreply.github.com>
2025-05-21 16:22:51 +0200

    Init libraries in mini-tour.ino

--- mini-tour/mini-tour.ino
+++ mini-tour/mini-tour.ino
@@ -1,9 +1,32 @@
+#include <AccelStepper.h>
+#include <MPU9250_asukiaaa.h>
+
+#define SDA_PIN 21
+#define SCL_PIN 22
+#define motorPin1 8
+#define motorPin2 9
+#define motorPin3 10
+#define motorPin4 11
+
+#define MotorInterfaceType 8 // 4 wire motor in half step mode (4096 steps)
+AccelStepper motor = AccelStepper(MotorInterfaceType, motorPin1, motorPin2, motorPin3, motorPin4);
+
+MPU9250_asukiaaa mpu;
+float aX, aY, aZ;
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  while(!Serial);
+
+  Wire.begin(SDA_PIN, SCL_PIN);
+  mpu.setWire(&Wire);
+
+  mpu.beginAccel();
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  int result;
+
 
 }
